c76d6bcc709596ae26cdac68f10ace012f2d59db
Andres Santiago Romero <andres.romero1@ibm.com>
2021-05-12 12:44:46 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blacksip-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'blacksip-store';
+}
